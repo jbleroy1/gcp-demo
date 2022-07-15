dee1889c23fc75c2fb5adcca8e41b84961bada87
jbleroy1 <jbleroy1@gmail.com>
2022-07-15 16:37:59 +0100

    init quarkus project

---
+++ quarkus-rest-app/src/main/java/com/google/cloud/data/User.java
@@ -0,0 +1,18 @@
+package com.google.cloud.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class User {
+
+    private String name;
+    private int age;
+
+    
+}
